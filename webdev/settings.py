234fea11decac2980ae13e6b54e38f8b847c5369
Alexis Daniel <alexisdanielmatto@gmail.com>
2025-08-15 14:14:13 -0300

    template

--- webdev/settings.py
+++ webdev/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICT_ROOT = BASE_DIR / 'static'
+STATICT_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'mycontacts/static',
 ]
